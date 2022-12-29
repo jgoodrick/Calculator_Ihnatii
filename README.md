b7f4ec94c722048094edad0f38a4dd88961ccece
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2022-12-29 15:09:52 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculator_Ihnatii
+
+A description of this package.
---
+++ Sources/Calculator_Ihnatii/Calculator_Ihnatii.swift
@@ -0,0 +1,6 @@
+public struct Calculator_Ihnatii {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
